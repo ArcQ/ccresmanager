70aafe82b1c3c42e8336455247a9b9fa10ef9248
Eddie Law <eddielaw296@gmail.com>
2016-11-05 02:27:53 -0400

    customize basic template configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,9 @@
 var config = {
-  entry: ['./src/main.js'],
+  entry: ['./src/manager/manager.js'],
   output: {
-    'filename': './generated/dist/calculator.js',
+    'filename': './generated/dist/ccresmanager.js',
     'libraryTarget': 'commonjs2',
-    'library': 'test'
+    'library': 'ccresmanager'
   },
   module: {
     loaders: [
@@ -24,10 +24,10 @@ var config = {
 
 if(process.env.NODE_ENV==='dev'){
   config.devtool = 'inline-source-map';
-  config.output.filename = './generated/build/calculator.js';
+  config.output.filename = './generated/build/ccresmanager.js';
 }
 else if(process.env.NODE_ENV==='dist'){
-  config.output.filename = './generated/dist/calculator.js';
+  config.output.filename = './generated/dist/ccresmanager.js';
 }
 
 module.exports = config;
